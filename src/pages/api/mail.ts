82606a67e0a3bf5eb9654c21e9656f3577ea1e15
Roshiori <brzrokuniyon@gmail.com>
2023-03-16 11:13:02 +0900

    feat: form api (editing)

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -2,7 +2,7 @@ import { createTransport } from "nodemailer";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 //メール送信先を配列に格納
-const ToList: string[] = [process.env.MAIL_RECEIVE1 as string];
+const ToList: string[] = [process.env.MAIL_RECEIVE1 as string, process.env.MAIL_RECEIVE2 as string];
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const transporter = createTransport({
@@ -19,11 +19,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     from: process.env.MAIL_SEND,
     to: ToList,
     subject: "お問い合わせありがとうございます!",
-    text: "お問い合わせを受け付けました",
+    text: "お問い合わせを受け付けました. 内容は以下のとおりです." + req.body,
   };
 
   try {
     await transporter.sendMail(mailData);
+    res.status(200).end()
   } catch (error) {
     console.log("エラー発生")
     throw error;
